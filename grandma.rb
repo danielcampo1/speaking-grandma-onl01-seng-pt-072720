bb090896015a21a19ce2d911665c80f37e98b851
daniel campo <campo_daniel94@yahoo.com>
2020-07-31 18:36:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,12 @@
 
 
 def speak_to_grandma(phrase)
- if phrase = I LOVE YOU GRANDMA!
+  if phrase = I LOVE YOU GRANDMA!
    puts "I LOVE YOU TOO PUMPKIN!"
 
-   elsif phrase = hi!
-   
-   puts "HUH!?, SPEAK UP, SUNNY!" 
- elseif phase = Hi Nana, how are you?
+  elsif phrase = hi!
+   puts "HUH!?, SPEAK UP, SUNNY!"
+
+  elsif phase = Hi Nana, how are you?
    end
+end
