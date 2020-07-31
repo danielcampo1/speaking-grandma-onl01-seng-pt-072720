d5aa8691c159d72933bd3d75c748955008349017
daniel campo <campo_daniel94@yahoo.com>
2020-07-31 18:52:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,10 +18,10 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
 
-  # elsif phrase == phrase.downcase
-  # puts "HUH!?, SPEAK UP, SONNY!"
+  elsif phrase == phrase.downcase
+  puts "HUH!?, SPEAK UP, SONNY!"
 
-  # elsif phrase == phrase.upcase
-  # puts "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938!"
    end
 end
